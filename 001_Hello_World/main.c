270f75d19ee7a66b4c4e4be431bfece72db8bd6e
Luigi De Simone <luigi.desimone3@gmail.com>
2025-10-22 16:12:02 +0200

    fix

--- 001_Hello_World/main.c
+++ 001_Hello_World/main.c
@@ -1,19 +1,22 @@
 
 #include <stdio.h>
 
-/*
- * 
- */
 int main() {
     
     printf("PRIMO ESEMPIO!\n");
     
     char s[10];
+
+    printf("Inserisci la prima parola!\n");
     scanf("%s", s);
-    printf("%s\n", s);
+    printf("Prima parola: %s\n", s);
+    
+    printf("Inserisci la seconda parola!\n");
     scanf("%s", s);
-    printf("%s\n", s);
+    printf("Seconda parola: %s\n", s);
+
+    printf("Inserisci la terza parola!\n");
     scanf("%s", s);
-    printf("%s\n", s);
+    printf("Terza parola: %s\n", s);
 }
 
